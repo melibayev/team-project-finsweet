fadca79d602695bc8cc18f0099c74e2fd8e7db32
“melibayev” <melibayefff1@gmail.com>
2023-05-08 00:36:07 +0500

    js qoshildi

---
+++ js/main.js
@@ -0,0 +1,49 @@
+// Navbar-open / Navbar-close
+function openNavbar() {
+    document.getElementById("navbar-responsive").style.right = "0";
+    document.body.classList.toggle("opacity");
+  }
+  function closeNavbar() {
+    document.getElementById("navbar-responsive").style.right = "-100%";
+  }
+  
+  document.getElementById("navbar-open").addEventListener("click", openNavbar);
+  document.getElementById("navbar-close").addEventListener("click", closeNavbar);
+
+
+  // BACKTOP function
+window.addEventListener("scroll", function () {
+    toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backtop.style.bottom = "50px";
+    } else {
+        backtop.style.bottom = "-50px";
+    }
+}
+
+
+// Navbar Shrink
+window.addEventListener("scroll", function () {
+    shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+    if (scrollY > 100) {
+        navbar.classList.add("navbar-shrink");
+    } else {
+        navbar.classList.remove("navbar-shrink");
+    }
+}
+
+
+// Aos function
+AOS.init({
+    duration: 1000
+});
